e37e4682614b57f9637c91d9a1598400b279f5ec
Vinh Gunter <vinh19030604@gmail.com>
2026-06-22 22:10:33 +0700

    Update bullet and scene

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -18,11 +18,28 @@ public class Bullet : MonoBehaviour
     void FixedUpdate()
     {
         rockRb.linearVelocity = direction * throwSpeed;
+        // Tính toán góc quay của dao dựa trên hướng đi
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90);
     }
     private void OnBecameInvisible()
     {
         gameObject.SetActive(false);
     } // Kiểm tra nếu object ra khỏi màn hình sẽ tự động trả về Pool.
 
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Bounder"))
+        {
+            gameObject.SetActive(false);
+        }
+        
+    }
 }
