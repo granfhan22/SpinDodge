a82559541dc015a7f68ebf9f01a545f768631e4e
Vinh Gunter <vinh19030604@gmail.com>
2026-06-23 21:40:14 +0700

    Update 23/6

--- Assets/Scripts/Bullet/SpawnBullet.cs
+++ Assets/Scripts/Bullet/SpawnBullet.cs
@@ -1,10 +1,10 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnBullet : MonoBehaviour
 {
-    [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private Transform playerPosition;
+    [SerializeField] private Transform playerPosition; 
     [Header("Camera bound property")]
     [SerializeField] private float topBound, bottomBound, leftBound, rightBound;
 
